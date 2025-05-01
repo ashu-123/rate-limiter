1f280b66228a4d9cf1f2f154b00fd18e487b02b3
Ashutosh <Ashutosh>
2025-05-01 16:37:54 +0530

    Removed unused import

--- src/test/java/com.rateLimiter/FixedWindowRateLimiterTest.java
+++ src/test/java/com.rateLimiter/FixedWindowRateLimiterTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FixedWindowRateLimiterTest {
 
