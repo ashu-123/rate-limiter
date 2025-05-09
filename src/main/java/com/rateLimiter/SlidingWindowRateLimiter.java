2a41a833e9ef5d9e216565807d36827f5eb4bcbd
Ashutosh <Ashutosh>
2025-05-09 12:00:32 +0530

    Added java doc

--- src/main/java/com/rateLimiter/SlidingWindowRateLimiter.java
+++ src/main/java/com/rateLimiter/SlidingWindowRateLimiter.java
@@ -6,6 +6,7 @@ import redis.clients.jedis.args.ExpiryOption;
 
 import java.util.Map;
 
+/** This class implements sliding window rate limiting algorithm using Jedis.*/
 public class SlidingWindowRateLimiter {
 
     private final Jedis jedis;
@@ -20,6 +21,12 @@ public class SlidingWindowRateLimiter {
         this.limit = limit;
     }
 
+    /**
+     * Checks if a client is within their allowed request limit.
+     * If yes, the request is allowed and the counter is updated. If not, the request is blocked.
+     * @param clientId the unique id of the client
+     * @return True if request is allowed. False if not.
+     */
     public boolean isAllowed(String clientId) {
         String key = "rate-limiter: " + clientId;
         Map<String, String> subWindowCounts = jedis.hgetAll(key);
